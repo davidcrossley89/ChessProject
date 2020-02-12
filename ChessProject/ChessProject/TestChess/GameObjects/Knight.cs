baf1879afdb1e481f1521bc2fd2853c7f98a7bc8
davidcrossley89 <60151908+davidcrossley89@users.noreply.github.com>
2020-02-12 14:49:55 -0800

    Add files via upload

---
+++ ChessProject/ChessProject/TestChess/GameObjects/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace GamePieces
+{
+    public class Knight : GamePiece
+    {
+        public Knight(int[] startingPos, Boolean white, string n)
+        {
+            
+            this.setPosition(startingPos);
+            this.setPossibleMoves(new int[][] { new int[]{ -2, -1 }, new int[] { -2, 1 }, new int[] { -1, -2 }, new int[] { -1, 2 }, new int[] { 1, -2 }, new int[] { 1, 2 }, new int[] { 2, -1 }, new int[] { 2, 1 } });
+            this.setWhite(white);
+            this.setName(n);
+            if (white) { this.setImage(Image.FromFile("WhiteKnight.jpg")); }
+            else { this.setImage(Image.FromFile("BlackKnight.jpg")); }
+
+        }
+
+    }
+}

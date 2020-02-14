14cc9ddc60f311637e9fac48bf911d56d9a1e1c2
davidcrossley89 <60151908+davidcrossley89@users.noreply.github.com>
2020-02-14 11:38:21 -0800

    Created Empty piece.  converted board.pieces to dictionary

---
+++ TestChess/GameObjects/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace GamePieces
+{
+    public class Knight : GamePiece
+    {
+        public Knight(int[] startingPos, Boolean white, string n)
+        {
+            
+            this.setPosition(startingPos);
+            this.setPossibleMoves(new int[][] { new int[]{ -2, -1 }, new int[] { -2, 1 }, new int[] { -1, -2 }, new int[] { -1, 2 }, new int[] { 1, -2 }, new int[] { 1, 2 }, new int[] { 2, -1 }, new int[] { 2, 1 } });
+            this.setWhite(white);
+            this.setName(n);
+            if (white) { this.setImage(global::TestChess.Properties.Resources.WhiteKnight); }
+            else { this.setImage(global::TestChess.Properties.Resources.BlackKnight); }
+
+        }
+
+    }
+}
